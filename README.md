171231adf7aeb848c3e1df0037fe5801adc96910
zinntikumugai <uesitananame55@gmail.com>
2020-05-16 18:02:08 +0900

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# VRCCamOverwriteController
+Avatarの画像アップロード時の画像にSDKのバージョンやらを追加できる様になるスクリプトです  
+
+## 使い方
+1. VRCSDKやAvatarなどインポートし、アップロード可能な状態にしておく
+1. unitypackageをインポート
+1. `zin3Tools/VRCCamOverwrite`の`VRCCOC`プレハブをSceneに追加
+1. Scene上の`VRCCOC`オブジェクトを選択し、Inspector上の`VRC Cam Overwrite Controller`の設定
+    - View SDK Version: SDKのバージョンを表示
+    - View Unity Version: Unityのバージョンを表示
+    - Write Texts: 末尾に文字列を追加します
+    - Cam Distance: カメラとの距離  
+        VRCCamがデフォルトでのおすすめ設定
+        - 0.5: Avatarの前あたり
+        - 50: Avatarの後ろあたり
+    - SDK File Path: SDKのバージョンが書かれたファイルのパス(構造変更している場合は修正してください)
+- 文字がかぶるのでVRCCamの高さを修正する
+- Build & Publishをする
+
+## その他
+やってることとしてはVRCamの前にUIを置いてるだけなので適当な画像を追加するなどもできると思います  
+同様のスクリプトとして[VRCCamController](https://github.com/kmnk/VRCCamController)([booth](https://booth.pm/ja/items/1296842)) があります
+## License
+MIT License
+## Update Log
+- 1.0 初期リリース
---
+++ VRCCamOverwriteController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRCCamOverwriteController : MonoBehaviour
+{
+    public bool viewSDKVersion = false;
+    public bool viewUnityVersion = false;
+    public string writeTexts;
+    private GameObject vrcCam;
+    private readonly string vrcCamName = "/VRCCam";
+    private GameObject canvas;
+    private string canvasName = "VRCCOC_Canvas";
+    public float camDistance = 50.0f;
+    private GameObject text;
+    private string textName = "VRCCOC_WriteText";
+    public string SDKFilePath = @"/VRCSDK/version.txt";
+    private string SDKVersion = "not found";
+
+    void Start()
+    {
+        // SDK version
+        string filepath = Application.dataPath + SDKFilePath;
+        if (File.Exists(filepath))
+        {
+            SDKVersion = File.ReadAllText(filepath);
+        }
+        else
+        {
+            Debug.Log("SDK Version File Path: " + filepath.ToString());
+            Debug.Log("SDK Version File: Faild.");
+        }
+
+        if (canvas == null)
+        {
+            canvas = GameObject.Find(canvasName);
+        }
+        if (text == null)
+        {
+            text = GameObject.Find(textName);
+        }
+    }
+
+    void Update()
+    {
+
+        if (vrcCam == null)
+        {
+            vrcCam = GameObject.Find(vrcCamName);
+            if (vrcCam == null)
+                return;
+        }
+
+        canvas.GetComponent<Canvas>().worldCamera = vrcCam.GetComponent<Camera>();
+        canvas.GetComponent<Canvas>().planeDistance = camDistance;
+
+        StringBuilder sb = new StringBuilder();
+        if (viewUnityVersion)
+        {
+            sb.AppendFormat("Unity: {0}", Application.unityVersion);
+            sb.AppendLine();
+        }
+        if (viewSDKVersion)
+        {
+            sb.AppendFormat("SDK: {0}", SDKVersion);
+            sb.AppendLine();
+        }
+        sb.Append(writeTexts);
+        text.GetComponent<Text>().text = sb.ToString();
+    }
+}
